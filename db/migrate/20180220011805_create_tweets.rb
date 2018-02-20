56484db537cafd3111e7e3530be425e1f7330cdf
hiroshi miyagi <miyahiro0730@hotmail.co.jp>
2018-02-21 02:38:57 +0900

    migrate修正-Drop削除-

--- db/migrate/20180220011805_create_tweets.rb
+++ db/migrate/20180220011805_create_tweets.rb
@@ -1,6 +1,6 @@
 class CreateTweets < ActiveRecord::Migration[5.1]
   def change
-    drop_table :tweets
+    # drop_table :tweets
     create_table :tweets do |t|
       t.string :content
       t.references :user, foreign_key: true

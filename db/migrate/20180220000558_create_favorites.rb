feebea1b418047289ff9c60915add87d121d17d4
hiroshi miyagi <miyahiro0730@hotmail.co.jp>
2018-02-21 02:33:05 +0900

    Procfile追加

--- db/migrate/20180220000558_create_favorites.rb
+++ db/migrate/20180220000558_create_favorites.rb
@@ -1,6 +1,6 @@
 class CreateFavorites < ActiveRecord::Migration[5.1]
   def change
-    drop_table :favorites
+    drop_table :favorites, force: :cascade
     create_table :favorites do |t|
       t.integer :user_id
       t.integer :tweet_id
